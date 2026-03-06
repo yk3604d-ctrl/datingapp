6595d4fcf752fe40da8e39dab1f6543134754be9
yusufkhan08070817 <khanyusuk340@gmail.com>
2026-03-07 02:11:12 +0530

    retry

--- utiiles/sendEmail.js
+++ utiiles/sendEmail.js
@@ -1,7 +1,10 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4, 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
